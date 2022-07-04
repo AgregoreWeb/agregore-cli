2f374951c03e185676f263fa147dd67ced6b17c3
mauve <ranger@mauve.moe>
2022-07-03 23:07:55 -0400

    Properly clean up test

--- test.js
+++ test.js
@@ -10,7 +10,7 @@ await agregore.init()
 
 const toFetch = 'https://blog.mauve.moe/esm.js'
 
-assert.doesNotReject(async () => {
+await assert.doesNotReject(async () => {
   const response = await agregore.fetch(toFetch)
 
   assert(response.ok, 'Able to fetch from HTTPS')
@@ -18,22 +18,25 @@ assert.doesNotReject(async () => {
   console.log(await response.text())
 }, 'Able to fetch')
 
-assert.doesNotThrow(() => {
+await assert.doesNotThrow(() => {
   const result = agregore.eval('400 + 20')
 
   assert.equal(result, 420, 'Got correct result from eval')
 }, 'Able to evaluate JS')
 
-assert.doesNotReject(async () => {
+await assert.doesNotReject(async () => {
   const module = await agregore.import(toFetch)
 
   module.default()
 }, 'Able to import module')
 
-assert.doesNotReject(async () => {
+await assert.doesNotReject(async () => {
   const p2pURL = 'hyper://blog.mauve.moe/esm.js'
 
   const module = await agregore.import(p2pURL)
 
   module.default()
 }, 'Able to import module from hyper://')
+
+await agregore.close()
+console.log('Done!')
